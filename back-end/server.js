0b85f7d7a5e1b1d6763527a363faa5779b357375
Ian Kirkham <imkirk56@gmail.com>
2020-12-01 15:46:30 -0700

    Added mongodb databases for users and posts

--- back-end/server.js
+++ back-end/server.js
@@ -9,9 +9,36 @@ app.use(bodyParser.urlencoded({
 
 const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+var user_db = mongoose.createConnection('mongodb://localhost:27017/users', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+var post_db = mongoose.createConnection('mongodb://localhost:27017/posts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const postSchema = new mongoose.Schema({
+  content: String,
+  replies: Array,
+  likes: Number,
+  favorite: Boolean,
+  date_posted: String,
+});
+
+const User = user_db.model('User', userSchema);
+const Post = post_db.model('Post', postSchema);
+
+app.get('/', async (req, res) => {
+  res.send("Hello World");
+});
+
+// add all api calls here
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
 
